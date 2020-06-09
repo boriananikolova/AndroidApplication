324adcbd81f35086c08c05dea1e645e90b9bc8c9
boriananikolova <boriana.nikolova7@gmail.com>
2020-06-09 20:06:33 +0300

    Google Sign In

--- app/src/main/java/com/example/androidapplication/MainActivity.java
+++ app/src/main/java/com/example/androidapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -12,13 +13,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private EditText username, password;
     private TextView attempts;
@@ -27,11 +34,25 @@ public class MainActivity extends AppCompatActivity {
     private TextView register;
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    SignInButton signInButton;
+    private GoogleApiClient googleApiClient;
+    private static final int SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+        signInButton = findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN);
+            }
+        });
 
         username = (EditText)findViewById(R.id.loginUsername);
         password = (EditText)findViewById(R.id.loginPassword);
@@ -90,5 +111,26 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+
+            if(result.isSuccess()){
+                startActivity(new Intent(MainActivity.this, PickActivity.class));
+                finish();
+            }else{
+                Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/androidapplication/PickActivity.java
+++ app/src/main/java/com/example/androidapplication/PickActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,14 +8,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class PickActivity extends AppCompatActivity {
+public class PickActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private Button singleplayer;
     private Button multiplayer;
     private Button logOutBtn;
     private FirebaseAuth firebaseAuth;
+    private GoogleApiClient googleApiClient;
+    private GoogleSignInOptions gso;
 
 
     @Override
@@ -27,10 +36,15 @@ public class PickActivity extends AppCompatActivity {
         logOutBtn = (Button)findViewById(R.id.logoutBtn);
         firebaseAuth = FirebaseAuth.getInstance();
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API, gso).build();
+
+
         logOutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 firebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(googleApiClient);
                 startActivity(new Intent(PickActivity.this, MainActivity.class));
             }
         });
@@ -48,5 +62,10 @@ public class PickActivity extends AppCompatActivity {
                 startActivity(new Intent(PickActivity.this, GameActivity.class));
             }
         });
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
